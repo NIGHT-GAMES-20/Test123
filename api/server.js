26876709210e766e7fee419f43472558dcc0de69
NIGHT-GAMES-20 <night20games@gmail.com>
2025-03-29 13:41:30 +0530

    Fix routing issue

--- api/server.js
+++ api/server.js
@@ -1,8 +1,18 @@
 const express = require("express");
+const path = require("path");
 const app = express();
 
+// Serve frontend files
+app.use(express.static(path.join(__dirname, "../public")));
+
+// API Route
 app.get("/api/data", (req, res) => {
     res.json({ message: "Hello from Vercel Backend!" });
+});
+
+// Serve index.html for all other routes (fixes 404)
+app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "../public/index.html"));
 });
 
 module.exports = app;
